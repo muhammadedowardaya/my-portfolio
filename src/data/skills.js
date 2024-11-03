d29b02f182fa46b0437006789d1910d4ef47790c
muhammadedowardaya <muhammadedowarday4@gmail.com>
2024-11-03 10:52:07 +0700

    init

---
+++ src/data/skills.js
@@ -0,0 +1,82 @@
+export const skills = [
+    {
+        title: 'HTML',
+        description: 'HTML is the standard markup language for creating web pages.',
+        iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg',
+    },
+    {
+        title: 'CSS',
+        description: 'CSS is the language we use to style an HTML document.',
+        iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg',
+    },
+    {
+        title: 'JAVASCRIPT',
+        description: 'JavaScript is a scripting or programming language that allows you to implement complex features on web pages.',
+        iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg',
+    },
+    {
+        title: 'GIT',
+        description: 'Git is a free and open source distributed version control system.',
+        iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg',
+    },
+    {
+        title: 'GITHUB',
+        description: 'GitHub is a code hosting platform for version control and collaboration.',
+        iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg',
+    },
+    {
+        title: 'NPM',
+        description: 'npm is a package manager for the JavaScript programming language.',
+        iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/npm/npm-original-wordmark.svg',
+    },
+    {
+        title: 'TAILWINDCSS',
+        description: 'Tailwind CSS is a utility-first CSS framework for rapidly building custom user interfaces.',
+        iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg',
+    },
+    {
+        title: 'SASS',
+        description: 'Sass is a CSS preprocessor.',
+        iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg',
+    },
+    {
+        title: 'ESLINT',
+        description: 'ESLint is a tool for identifying and reporting on patterns found in ECMAScript/JavaScript code.',
+        iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/eslint/eslint-original.svg',
+    },
+    {
+        title: 'VITE',
+        description: 'Vite is a build tool for modern web applications.',
+        iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg',
+    },
+    {
+        title:'VITEST',
+        description: 'Vitest is a test runner for Vite.',
+        iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitest/vitest-original.svg',
+    },
+    {
+        title: 'PLAYWRIGHT',
+        description: 'Playwright is a headless browser testing framework.',
+        iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/playwright/playwright-original.svg',
+    },
+    {
+        title:'TYPESCRIPT',
+        description: 'TypeScript is a typed superset of JavaScript.',
+        iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg',
+    },
+    {
+        title: 'REACTJS',
+        description: 'React is a JavaScript library for building user interfaces.',
+        iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg',
+    },
+    {
+        title: 'NEXTJS',
+        description: 'Next.js is a React framework for building server-rendered applications.',
+        iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg',
+    },
+    {
+        title: 'ASTRO',
+        description: 'Astro is a framework for building web applications.',
+        iconUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/astro/astro-original.svg',
+    }
+]
---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+        screens: {
+            'mobile' : '400px',
+            'tablet': '760px',
+            // => @media (min-width: 640px) { ... }
+      
+            'laptop': '1024px',
+            // => @media (min-width: 1024px) { ... }
+      
+            'desktop': '1280px',
+            // => @media (min-width: 1280px) { ... }
+        },
+		extend: {},
+	},
+	plugins: [],
+}
