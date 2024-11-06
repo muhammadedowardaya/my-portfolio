ead9770435352e10b8c72b1f21ae78e78b20245e
muhammadedowardaya <muhammadedowardaya@users.noreply.github.com>
2024-11-06 02:01:02 +0000

    deploy: ebac8a7667ad86a07395793046033dbfc328729d

---
+++ src/data/projects.js
@@ -0,0 +1,32 @@
+export const projects = [
+	{
+		title: 'WDY MOVIES',
+		description:
+			'Lihat daftar Film, lihat trailernya, lakukan streaming (jika tersedia), tambahkan Film ke Favorite, cari Film yang kamu suka, seleksi Film sesuai genre, urutkan berdasarkan populer, terbaru dan lainnya.',
+		href: 'https://wdymovies.vercel.app/',
+	},
+	{
+		title: 'NOTES APP',
+		description:
+			'Aplikasi catatan, kamu bisa membuat catatan, mencari, mengarsipkan, keluar dari arsip atau menghapusnya',
+		href: 'https://medow-notes-app.vercel.app/',
+	},
+	{
+		title: '25 plus 5',
+		description:
+			'Kelola waktu!, 25 plus 5 artinya 25 menit bekerja dan 5 menit istirahat, menit tersebut dapat disesuaikan. Dengan time management seperti ini diharapkan bisa membantu kita untuk bisa lebih produktif.',
+		href: 'https://25plus5.vercel.app/',
+	},
+	{
+		title: 'Random Quotes Machine',
+		description:
+			'Sebuah mesin penghasil kutipan kata kata atau quotes secara acak',
+		href: 'https://random-quote-machine-ten-psi.vercel.app/',
+	},
+	{
+		title: 'Markdown Previewer',
+		description:
+			'Buat Markdown lalu lihat hasilnya secara langsung',
+		href: 'https://markdown-previewer-one-ecru.vercel.app/',
+	},
+];
---
+++ src/utils/changeActiveNavLink.js
@@ -0,0 +1,26 @@
+const callback = (entries) => {
+	const navListItems = document.querySelectorAll('.nav-list .nav-item');
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			navListItems.forEach((navLink) => {
+				navLink.classList.remove('active');
+
+                if(navLink.querySelector(`a`).getAttribute('href') === `#${entry.target.id}`){
+                    navLink.classList.add('active')
+                }
+			});
+
+			console.info('terlihat id :', entry.target.id);
+		}
+	});
+};
+
+const observer = new IntersectionObserver(callback, {
+	root: null,
+	threshold: 0.1,
+});
+
+const sections = document.querySelectorAll('section');
+sections.forEach((section) => {
+	observer.observe(section);
+});
